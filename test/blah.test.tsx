3df44027b1e3a01ba24fa1929e004866257a80e1
Nico Gonzalez <nicogonzalez2008@gmail.com>
2023-02-12 21:36:21 -0300

    test fixed

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,15 +1,13 @@
 /* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react'
 import React from 'react'
-import { ExampleComponent } from '../src/index'
+import { Example } from '../src/example'
 
 describe('ExampleComponent', () => {
   test('renders without crashing', () => {
-    render(
-      <ExampleComponent label="Testing component" className='test' />
-    )
+    render(<Example />);
 
-    const element = screen.getByText('Testing component')
-    expect(element).toHaveClass('test')
+    const element = screen.getByText('Example component');
+    expect(element).toBeInTheDocument()
   })
 })
