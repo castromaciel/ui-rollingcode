0dbf85ffb29ee27b12ca5b0cc75d12b4d8f7746e
castromaciel <castromaciel00@gmail.com>
2023-02-12 23:40:19 -0300

    feat: Add basic cards

---
+++ src/structure/Card/Card.test.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+import { fireEvent, render, screen } from '@testing-library/react'
+import React from 'react'
+import { Card } from '../Card'
+
+const classicCard = { 
+  buttonRight: 'Show me more',
+  description: 'Lorem ipsum dolor sit amet, consectetur adip',
+  image: 'https://fastly.picsum.photos/id/237/300/300',
+  onClickRight: () => jest.fn(),
+  title: 'Classic Card'
+}
+
+const productCard = {
+  buttonRight: 'Show me more',
+  description: 'Lorem ipsum dolor sit amet, consectetur adip',
+  image: 'https://fastly.picsum.photos/id/237/300/300',
+  onClickRight: () => jest.fn(),
+  price: '2000',
+  title: 'Classic Card'
+}
+
+describe('Card', () => {
+  test('Renders classic', () => {
+    render(<Card {...classicCard} />)
+
+    const element = screen.getByText('Classic Card')
+    expect(element).toBeInTheDocument()
+  })
+
+  test('Renders product card with price "$2000" ', () => {
+    render(<Card {...productCard} mode="product" />)
+
+    const element = screen.getByText('$2000')
+    expect(element).toBeInTheDocument()
+  })
+
+  test('Click "Show me more" button', () => {
+    const handleClick = jest.fn()
+
+    render(<Card {...productCard} mode="product" onClickRight={handleClick} />)
+
+    const buttonElement = screen.getByText('Show me more')
+    fireEvent.click(buttonElement)
+
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+})
--- src/structure/Card/Card.tsx
+++ src/structure/Card/Card.tsx
@@ -1,5 +1,79 @@
-import React from 'react'
+import React, { FC, MouseEventHandler } from 'react'
+import cardStyles from './card.module.scss'
 
-const Card = () => <h1>Card component</h1>
+export type CardModes = | 'classic' | 'product'
+
+export interface CardProps {
+  buttonRight: string
+  description: string
+  image: string
+  title: string
+  onClickRight: MouseEventHandler<HTMLButtonElement>
+  
+  altImage?: string
+  buttonLeft?: string
+  className?: string
+  id?: string
+  mode?: CardModes
+  onClickLeft?: MouseEventHandler<HTMLButtonElement>
+  style?: React.CSSProperties
+  price?: string | number
+  symbol?: string
+}
+
+const Card: FC<CardProps> = ({
+  altImage,
+  buttonLeft,
+  buttonRight,
+  className = '',
+  description,
+  id,
+  image,
+  mode = 'classic',
+  onClickLeft,
+  onClickRight,
+  style,
+  symbol = '$',
+  price = 100,
+  title
+}) => (
+  <article id={id} className={`${className} ${cardStyles.card}`} style={style}>
+    <header className={`${cardStyles.img_container}`}>
+      <img src={image} alt={altImage} />
+    </header>
+
+    <section className={`${cardStyles.card_body}`}>
+
+      <h2>{title}</h2>
+
+      <p className={`${cardStyles.description}`}>{description}</p>
+
+      {
+        mode === 'classic'
+          ? (
+            <div className={`${cardStyles.buttons_container}`}>
+              { buttonLeft
+                ? <button type="button" className={`${cardStyles.button_left}`} onClick={onClickLeft}>{buttonLeft}</button>
+                : null }
+              <button type="button" onClick={onClickRight}>{buttonRight}</button>
+            </div>
+          )
+          : (
+            <>
+              <hr />
+              <div className={`${cardStyles.buttons_price_container}`}>
+                <p className={`${cardStyles.price}`}>
+                  {symbol} 
+                  {price}
+                </p>
+                <button type="button" onClick={onClickRight}>{buttonRight}</button>
+              </div>
+            </>
+          )
+      }
+
+    </section>
+  </article>
+)
 
 export default Card
